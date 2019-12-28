59110e9093e82adaf1d5998a10114738716fe100
hello413 <2827717859@qq.com>
2019-12-28 22:20:12 +0800

    practical

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var num = 25;
+if (num <= 20) {
+    $("#next")[0].innerHTML = "";
+    if (num <= 4) {
+        $("ul>#a:gt(0)").html("<div style='width:80px'></div>");
+    } else if (num <= 8) {
+        $("ul>#a:gt(1)").html("");
+    } else if (num <= 12) {
+        $("ul>#a:gt(2)").html("");
+    } else if (num <= 16) {
+        $("ul>#a:gt(3)").html("");
+    } else {
+        $("ul>#a:gt(4)").html("");
+    }
+} else {
+    $("#next").click(function() {
+        if ($("ul>#a>#num")[4].innerHTML !== parseInt(num / 4 + 1).toString()) {
+            for (let i = 0; i < 5; i++) {
+                $("ul>#a>#num")[i].innerHTML = (Number($("ul>#a>#num")[i].innerHTML) + 1).toString();
+            }
+        }
+        if ($("ul>#a>#num")[0].innerHTML !== "1") {
+            $("#first").removeClass("disabled");
+        }
+        if ($("ul>#a>#num")[4].innerHTML === parseInt(num / 4 + 1).toString()) {
+            $("#next").addClass("disabled");
+        }
+    })
+    $("#first").click(function() {
+        if ($("ul>#a>#num")[0].innerHTML !== "1") {
+            for (let i = 0; i < 5; i++) {
+                $("ul>#a>#num")[i].innerHTML = (Number($("ul>#a>#num")[i].innerHTML) - 1).toString();
+            }
+        }
+        if ($("ul>#a>#num")[0].innerHTML === "1") {
+            $("#first").addClass("disabled");
+        }
+        if ($("ul>#a>#num")[4].innerHTML !== parseInt(num / 4 + 1).toString()) {
+            $("#next").removeClass("disabled");
+        }
+    })
+}
